c26150cf10c28561eae61d8b181e508b5524d6ef
abhayvatoo <abhay.vatoo.jal@gmail.com>
2024-02-11 10:27:12 -0500

    Add support for system theme preference

--- src/components/theme-menu.tsx
+++ src/components/theme-menu.tsx
@@ -1,4 +1,4 @@
-import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import { SunIcon, MoonIcon, DesktopIcon } from "@radix-ui/react-icons";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -8,38 +8,63 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import useTheme from "@/hooks/useTheme";
+import { Theme } from "@/contexts/theme-context";
+
+function getIconForCurrentTheme(theme: Theme) {
+  const moon = <MoonIcon className="h-5 w-5 sm:h-4 sm:w-4" />;
+  const sun = <SunIcon className="h-5 w-5 sm:h-4 sm:w-4" />;
+
+  switch (theme) {
+    case "dark":
+      return moon;
+    case "light":
+      return sun;
+    case "system":
+      return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? moon
+        : sun;
+  }
+}
 
 export default function ThemeMenu() {
-  const { theme, updateTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size="icon" variant="ghost">
-          {theme === "dark" ? (
-            <MoonIcon className="h-5 w-5 sm:h-4 sm:w-4" />
-          ) : (
-            <SunIcon className="h-5 w-5 sm:h-4 sm:w-4" />
-          )}
+          {getIconForCurrentTheme(theme)}
         </Button>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent className="min-w-28">
         <DropdownMenuItem>
           <Button
             className="p-1"
             variant="ghost"
-            onClick={() => updateTheme("light")}
+            onClick={() => setTheme("light")}
           >
             <SunIcon className="mr-2 h-4 w-4" /> Light
           </Button>
         </DropdownMenuItem>
+
         <DropdownMenuItem>
           <Button
             className="p-1"
             variant="ghost"
-            onClick={() => updateTheme("dark")}
+            onClick={() => setTheme("dark")}
           >
             <MoonIcon className="mr-2 h-4 w-4" /> Dark
+          </Button>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Button
+            className="p-1"
+            variant="ghost"
+            onClick={() => setTheme("system")}
+          >
+            <DesktopIcon className="mr-2 h-4 w-4" /> System
           </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
